65fbb40369f4beb5683c7fd7011cbf12eb38b7c2
Rodrigo <rodrihercar24@gmail.com>
2025-07-24 09:38:19 -0600

    primer menu

---
+++ main.py
@@ -0,0 +1,11 @@
+
+while True:
+    print("----Bienvenido al menu----")
+    print("1. Ingrese numeros:\n"
+          "2. Calcular area de un triangulo\n"
+          "3. Verificar si un numero es par o impar\n"
+          "4. Calcular promedio de n calificaciones\n"
+          "5. Conocer numero mayor y el menor\n"
+          "6. salir del programa")
+    select = input("Ingrese una opcion: ")
+    match select:
