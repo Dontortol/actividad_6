9978d059014a5253ec14d8d3d461d4865c99d2c4
Rodrigo <rodrihercar24@gmail.com>
2025-07-24 09:49:42 -0600

    primera opcion del menu, cambios necesarios en las funciones

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+suma = []
 
 while True:
     print("----Bienvenido al menu----")
-    print("1. Ingrese numeros:\n"
+    print("1. Ingresar numeros\n"
           "2. Calcular area de un triangulo\n"
           "3. Verificar si un numero es par o impar\n"
           "4. Calcular promedio de n calificaciones\n"
@@ -9,3 +10,25 @@ while True:
           "6. salir del programa")
     select = input("Ingrese una opcion: ")
     match select:
+        case "1":
+            while True:
+                print("1. Ingresar numero:\n"
+                      "2. ver suma total, promedio, cuantos positivos y negativos \n"
+                      "3. regresar al menu\n")
+                select1 = input("Ingrese una opcion: ")
+                match select1:
+                    case "1":
+                        num = int(input("Ingrese un numero: "))
+                        suma.append(num)
+                    case "2":
+                        print(f"La suma total es = {sum(suma)}\n"
+                              f"El promedio de = {sum(suma)/len(suma)} \n"
+                              f"la cantidad de positivos es de = {len(suma) > 0} \n"
+                              f"la cantidad de negativos es de = {len(suma) < 0} \n")
+                        break
+                    case "3":
+                        print("Regresando al menu")
+                        break
+        case "6":
+            print("Saliendo del programa")
+            break
